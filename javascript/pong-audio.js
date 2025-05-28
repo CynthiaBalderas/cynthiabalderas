5555c40c8250c824f381dd7b67cd4bfe1538a434
CynthiaBalderas <c.mbalderas2@gmail.com>
2025-05-28 12:06:59 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -44,9 +44,8 @@ export var soundArray = []; //list of sounds loaded
 //Here is where all the Sound File Players Start
 
 export var wallSound = new soundFile("soft transition.mp3"); // load sound
-wallSound.setVolume(0.2); // set volume to 20%
+wallSound.setVolume(0.1); // set volume to 10%
 soundArray.push(wallSound); // add sound to list of sounds
-
 
 export var paddleSound = new soundFile("birds chirping.mp3");
 soundArray.push(paddleSound);
